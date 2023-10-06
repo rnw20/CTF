914a326ef165ea806b2669e320ddb70e393922e5
rnw20 <gd9099@gmail.com>
2023-10-06 18:15:23 -0400

    update solve.py

--- BuckeyeCTF2023/Crypto/Electronical/solve.py
+++ BuckeyeCTF2023/Crypto/Electronical/solve.py
@@ -1,21 +1,31 @@
 import requests
-url = 'https://electronical.chall.pwnoh.io/encrypt'
 
-plaintext = ""
+url = 'https://electronical.chall.pwnoh.io/encrypt'
+flag = ""
+
 for i in range(47, 0, -1):
+    # create message
     message = 'a' * i
     x = requests.get(url, params = {"message": message})
     resp = x.text
+
+    # split into 16-byte blocks
     parts = [resp[i:i+32] for i in range(0, len(resp), 32)]
+
+    # store target hex sequence
     matchthis = parts[2]
+
+    # iterate through possible characters
     for j in range(33, 127):
-        message1 = message + plaintext + chr(j)
+        # append known flag to message and test a character
+        message1 = message + flag + chr(j)
         x = requests.get(url, params = {"message": message1})
         resp = x.text
         parts = [resp[i:i+32] for i in range(0, len(resp), 32)]
         check = parts[2]
+        
+        # check if the two hex sequences are the same
         if check == matchthis:
-            plaintext += chr(j)
-            print(plaintext)
+            flag += chr(j)
+            print(flag)
             break
-print(plaintext)
